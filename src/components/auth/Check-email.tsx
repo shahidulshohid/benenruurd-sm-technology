0cb8c0ff72e26bf28ac37962adbf9d8215f7c1c8
Shahidul Shohid <shahidulislamshohid7@gmail.com>
2025-07-03 01:25:50 +0600

    check email page updated

--- src/components/auth/Check-email.tsx
+++ src/components/auth/Check-email.tsx
@@ -15,7 +15,7 @@ function CheckEmailForm() {
             <div className="flex-1 flex items-center justify-center p-8 bg-white">
                 <div className="w-full max-w-md space-y-6">
                     <div className="text-left">
-                        <h1 className="text-3xl font-bold text-[#22222F] mb-2">Check your email</h1>
+                        <h1 className="text-2xl lg:text-4xl font-semibold text-[#22222F] mb-2">Check your email</h1>
                         <p className="text-sm text-[#8588AB]">
                             A confirmation email has just been sent to the email address <br/> you provided. Click the link in this email to reset your <br/> password.
                         </p>
@@ -24,7 +24,7 @@ function CheckEmailForm() {
                         <Link href="/login">
                         <button
                             type="submit"
-                            className="w-full bg-[#0D5AE8] text-white py-2 rounded-md hover:bg-[#0D5AE8] text-sm font-semibold cursor-pointer transition"
+                            className="w-full bg-[#0D5AE8] text-white py-2 rounded-md hover:bg-[#0D5AE8] text-sm font-semibold transition cursor-pointer"
                         >
                             Log in
                         </button>
